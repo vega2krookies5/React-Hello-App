502a80c4df1701aef1f75233219613c1290783df
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-23 15:25:38 +0900

    func handleChange call

--- src/components/MyComponentFunc.jsx
+++ src/components/MyComponentFunc.jsx
@@ -1,17 +1,17 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 //Function 형태의 컴포넌트
-const MyComponentFunc = ({name,children}) => {
+const MyComponentFunc = ({ name, children }) => {
     //useState() Hook 사용
     const [value, setValue] = useState(0);
-    
+
     const [inputs, setInputs] = useState({
         username: '',
         message: ''
     });
 
     //비구조화 할당
-    const {username, message} = inputs;
+    const { username, message } = inputs;
 
     //Event Handler 함수
     const handleChange = (e) => {
@@ -19,7 +19,7 @@ const MyComponentFunc = ({name,children}) => {
             [e.target.name]: e.target.value
         });
     } //handleChange
-    
+
     return (
         <div>
             <h2>함수 형태의 컴포넌트</h2>
@@ -28,6 +28,12 @@ const MyComponentFunc = ({name,children}) => {
             <p>상태변수 value = {value}</p>
             <button onClick={() => setValue(value + 1)}>증가</button>
             <button onClick={() => setValue(value - 1)}>감소</button>
+            <br />
+            <p>상태변수 message = {message}</p>
+            <input name="message" value={message} onChange={handleChange} />
+            <p>상태변수 username = {username}</p>
+            <input name="username" value={username} onChange={handleChange} />
+
         </div>
     );
 };
