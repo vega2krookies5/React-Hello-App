07e37285fc6e141e3847c4a200808670220b4096
vega2krookies5 <vega2krookies5@gmail.com>
2026-03-23 13:19:05 +0900

    함수형props pass

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,9 @@ const App = () => {
       <MyComponent  name="리액트" />
       <MyComponent age={20} />
       <hr/>
-      <MyComponentFunc name="리액트함수형" />
+      <MyComponentFunc name="리액트함수형">
+        <p>함수형 컴포넌트의 하위 엘리먼트</p>
+      </MyComponentFunc>  
     </>
   );
 };
